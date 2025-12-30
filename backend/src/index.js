21f969cc1b16c033b8e578e1b9fa3f014709d1ca
Arpan Mondal <arpan954mondal@gmail.com>
2025-12-31 04:13:58 +0530

    leave request backend

--- backend/src/index.js
+++ backend/src/index.js
@@ -8,6 +8,7 @@ import authRouter from "./routes/authRoutes.js";
 import userRouter from "./routes/userRoutes.js";
 import adminRouter from "./routes/adminRoutes.js";
 import orgRouter from "./routes/orgRoutes.js";
+import leaveRouter from "./routes/leaveRoutes.js";
 
 const app = express();
 
@@ -38,6 +39,7 @@ app.use("/api/auth", authRouter);
 app.use("/api/user", userRouter);
 app.use("/api/admin", adminRouter);
 app.use("/api/org", orgRouter);
+app.use("/api/leave", leaveRouter);
 
 // Listen for incoming connections
 const PORT = process.env.PORT;
